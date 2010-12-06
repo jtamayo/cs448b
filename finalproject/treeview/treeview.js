cdb5db7b9485a2254346550ed3a2e611f270e4bf
Juan Manuel Tamayo <jtamayo@cs.stanford.edu>
2010-12-05 23:22:21 -0800

    Added sorting by indegree, outdegree and assymetry

--- finalproject/treeview/treeview.js
+++ finalproject/treeview/treeview.js
@@ -1,29 +1,4 @@
 function funkysort(input){
-	var array = input.slice(0,input.length)
-    var funkysort = new Array();
-    // Add the words to the right panel. Sort them by prevalence 
-    array.sort(function(a, b){
-		if (a.count === b.count) {
-			return (a.node < b.node) ? -1 : 1  
-		} else {
-			return a.count - b.count
-		}
-    })
-    if (array.length === 46) 
-        array = array.slice(1, 45);
-    else 
-        if (array.length >= 47) 
-            array = array.slice(2, 45);
-    var j = 0;
-    for (i = 0; i < array.length; i += 2) {
-        funkysort[j] = array[i];
-        j++;
-    }
-    i = (array.length % 2 === 0) ? array.length - 1 : array.length - 2;
-    for (; i > 0; i -= 2) {
-        funkysort[j] = array[i];
-        j++;
-    }
     return input.slice(0, input.length).sort(function(a, b){
 		if (a.count === b.count) {
 			return (a.node < b.node) ? -1 : 1  
@@ -37,7 +12,7 @@ function funkysort(input){
 function runsum(input) {
 	var i;
 	var sum = 0;
-	var output = new Array();
+	var output = new Array(input.length);
 	for (i = 0; i < input.length; i++) {
 		sum = sum + input[i].count
 		output[i] = sum
